c952ecd2e0642a0f2fd6dc0a1a1b76cb5b536f08
GalangYuhu <galangmrd09@gmail.com>
2024-09-19 01:37:32 +0700

    feat: added component headers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/">
+                                <Link to="/openMe">
                                     Open Me! <LuStore />
                                 </Link>
                             </li>
