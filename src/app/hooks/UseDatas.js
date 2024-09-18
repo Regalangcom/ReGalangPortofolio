b17e73475b43f8e18eebdb8d710d3f4c2234fed8
GalangYuhu <galangmrd09@gmail.com>
2024-09-19 01:36:02 +0700

    feat: added hook apiClient

--- src/app/hooks/UseDatas.js
+++ src/app/hooks/UseDatas.js
@@ -1,26 +1,51 @@
 import { useQuery } from "react-query";
-import { dProvincies, dCity } from "../lib/Api.js";
+import { dProvincies, dCity, dKecamatan, dDesa } from "@eli/app/lib/Api.js";
 
-export const keyProv = "provincies";
-export const keyCity = "City";
+export const keyProv = "provinces";
+export const keyCity = "cities";
+export const keyKecamatan = "kecamatans";
+export const keyDesa = "desa";
 
 const useProvinces = (opts = {}) => {
-  const { data: provincies = [], ...rest } = useQuery({
+  const { data: provinces = [], ...rest } = useQuery({
     queryKey: [keyProv],
     queryFn: dProvincies,
     ...opts,
   });
 
-  return { provincies, ...rest };
+  return { provinces, ...rest };
 };
 
 const useCity = (id_City, opts = {}) => {
-  const { data: City = [], ...rest } = useQuery({
+  const { data: cities = [], ...rest } = useQuery({
     queryKey: [keyCity, id_City],
     queryFn: () => dCity(id_City),
+    enabled: !!id_City,
     ...opts,
   });
 
-  return { City, ...rest };
+  return { cities, ...rest };
 };
-export { useProvinces, useCity };
+
+const useKecamatan = (id_Kec, opts = {}) => {
+  const { data: kecamatan = [], ...rest } = useQuery({
+    queryKey: [keyKecamatan],
+    queryFn: () => dKecamatan(id_Kec),
+    enabled: !!id_Kec,
+    ...opts,
+  });
+  return { kecamatan, ...rest };
+};
+
+const useDesa = (id_Desa, opts = {}) => {
+  const { data: desa = [], ...rest } = useQuery({
+    queryKey: [keyDesa, id_Desa],
+    queryFn: () => dDesa(id_Desa),
+    enabled: !!id_Desa,
+    ...opts,
+  });
+
+  return { desa, ...rest };
+};
+
+export { useProvinces, useCity, useKecamatan, useDesa };
