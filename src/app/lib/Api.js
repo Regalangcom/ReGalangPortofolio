78d95b13c416b4e0f8d601703f3d9c07d8ab478d
GalangYuhu <galangmrd09@gmail.com>
2024-09-19 14:35:14 +0700

    fix: added witCredentials: false

--- src/app/lib/Api.js
+++ src/app/lib/Api.js
@@ -1,21 +1,26 @@
 import { API } from "@eli/app/config/ApiClient.js";
-
 export const dProvincies = async () => {
-  const response = await API.get("/provinces");
+  const response = await API.get("/provinces", { withCredentials: false });
   return response.data;
 };
 
 export const dCity = async (Id_provinsi) => {
-  const response = await API.get(`/regencies_of/${Id_provinsi}`);
+  const response = await API.get(`/regencies_of/${Id_provinsi}`, {
+    withCredentials: false,
+  });
   return response.data;
 };
 
 export const dKecamatan = async (id_Kec) => {
-  const response = await API.get(`/districts_of/${id_Kec}`);
+  const response = await API.get(`/districts_of/${id_Kec}`, {
+    withCredentials: false,
+  });
   return response.data;
 };
 
 export const dDesa = async (id_Desa) => {
-  const response = await API.get(`/villages_of/${id_Desa}`);
+  const response = await API.get(`/villages_of/${id_Desa}`, {
+    withCredentials: false,
+  });
   return response.data;
 };
